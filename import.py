0cb38ad88ced18b48984cc81f633fc921d542be7
Jezer <jezer.vallivattam@students.iiserpune.ac.in>
2020-05-03 00:28:18 +0530

    second commit

--- import.py
+++ import.py
@@ -13,10 +13,17 @@ if engine.dialect.has_table(engine, "books"):
     db.execute("DROP TABLE books;")
 db.execute("CREATE TABLE books (isbn VARCHAR PRIMARY KEY, title VARCHAR, author VARCHAR, year INT);")
 
+if not engine.dialect.has_table(engine, "users"):
+    db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR, key VARCHAR, salt VARCHAR);")
+
+if not engine.dialect.has_table(engine, "reviews"):
+    db.execute("CREATE TABLE reviews (id SERIAL PRIMARY KEY, isbn VARCHAR, rating INTEGER, review VARCHAR, user_id INTEGER REFERENCES users);")
+
 firstRow=1
 
 for isbn, title, author, year in reader:
     if firstRow==0:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year);", {'isbn':isbn, 'title':title, 'author':author, 'year':year})
     else: firstRow=0
+    
 db.commit()
